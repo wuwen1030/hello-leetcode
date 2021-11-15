b7158728bc9f1947cc9903197f48d9556647b617
wuwen1030 <wuwen1030@126.com>
2021-11-15 15:45:40 +0800

    feat: add solution [Generate Parentheses]

--- hello-leetcode.playground/Pages/GenerateParentheses.xcplaygroundpage/Contents.swift
+++ hello-leetcode.playground/Pages/GenerateParentheses.xcplaygroundpage/Contents.swift
@@ -3,8 +3,29 @@
 import Foundation
 
 class Solution {
+    var ans = [String]()
     func generateParenthesis(_ n: Int) -> [String] {
+        ans.removeAll()
+        var str = [Character](repeating: " ", count: 2 * n)
+        dfs(n, 0, 0, &str)
+        return ans
+    }
+    
+    func dfs(_ n: Int, _ leftCount: Int, _ rightCount: Int, _ str: inout [Character]) {
+        if (leftCount + rightCount) >= 2 * n {
+            ans.append(String(str))
+            return
+        }
         
+        if leftCount < n {
+            str[leftCount+rightCount] = "("
+            dfs(n, leftCount + 1, rightCount, &str)
+        }
+        
+        if leftCount > rightCount {
+            str[leftCount+rightCount] = ")"
+            dfs(n, leftCount, rightCount + 1, &str)
+        }
     }
 }
 //: [Next](@next)
