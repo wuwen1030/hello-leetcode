cb56549f3e95b7bba274be533288c3f7031586e0
BIN XIA <wuwen1030@126.com>
2021-11-17 22:35:48 +0800

    feat: Data structure utils

--- hello-leetcode.playground/Sources/ListNode.swift
+++ hello-leetcode.playground/Sources/ListNode.swift
@@ -1,9 +1,34 @@
 import Foundation
+
 public class ListNode {
-    public var val: Int
+    public var val: Int = 0
     public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    public init(_ val: Int) {
+        self.val = val
+    }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
 }
+
+public func linkedList(_ nums: [Int]) -> ListNode? {
+    var node: ListNode?
+    for num in nums.reversed() {
+        node = ListNode(num, node)
+    }
+    return node
+}
+
+public func printLinkedList(_ head: ListNode?) -> [Int] {
+    var node = head
+    var values = [Int]()
+    while let nnNode = node {
+        values.append(nnNode.val)
+        node = node?.next
+    }
+    return values
+}
+
 
--- hello-leetcode.playground/Sources/TreeNode.swift
+++ hello-leetcode.playground/Sources/TreeNode.swift
@@ -4,12 +4,62 @@ public class TreeNode {
     public var val: Int = 0
     public var left: TreeNode?
     public var right: TreeNode?
+    
     public init(_ val: Int) {
         self.val = val
     }
+    
     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
         self.val = val
         self.left = left
         self.right = right
     }
 }
+
+public func binaryTree(_ nums: [Int?]) -> TreeNode? {
+    if nums.compactMap({$0}).count == 0 {
+        return nil
+    }
+    var queue = [TreeNode?]()
+    guard let rootVal = nums.first as? Int else {
+        return nil
+    }
+    let root = TreeNode(rootVal)
+    queue.append(root)
+    var k = 1
+    while queue.count != 0, k < nums.count {
+        if let node = queue.first as? TreeNode {
+            let left = nums[k] != nil ? TreeNode(nums[k]!) : nil
+            queue.append(left)
+            k = k + 1
+            let right = nums[k] != nil ? TreeNode(nums[k]!) : nil
+            queue.append(right)
+            k = k + 1
+            node.left = left
+            node.right = right
+        }
+        queue.removeFirst()
+        
+    }
+    return root
+}
+
+public func printBinaryTree(_ root: TreeNode?) -> [Int?] {
+    var values = [Int?]()
+    var queue = [TreeNode?]()
+    queue.append(root)
+    while queue.count != 0 {
+        if let node = queue.first as? TreeNode {
+            values.append(node.val)
+            if node.left != nil || node.right != nil {
+                queue.append(node.left)
+                queue.append(node.right)
+            }
+        } else {
+            values.append(nil)
+        }
+        queue.removeFirst()
+    }
+    return values
+}
+
